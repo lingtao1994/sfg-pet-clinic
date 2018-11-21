1d8bc042244cf20797868d72227a3f0e5366dd27
lingtao <lingtaoshui@gmail.com>
2018-11-20 21:36:49 -0600

    Adding Vet Index Page and controller. closes #7.

---
+++ pet-clinic-web/src/main/java/guru/springframe/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,19 @@
+package guru.springframe.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author LT
+ * @Date on 2018/11/20
+ */
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(){
+
+        return "vets/index";s
+    }
+}
