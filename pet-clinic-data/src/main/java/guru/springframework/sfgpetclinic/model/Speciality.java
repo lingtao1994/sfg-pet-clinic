b1d1006c02a6ec0b30dbc0db7637564b3d379294
lingtao <lingtaoshui@gmail.com>
2018-11-25 17:01:02 -0600

    update vet, specialty with jpa. closes #36

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author LT
  * @Date on 2018/11/25
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
