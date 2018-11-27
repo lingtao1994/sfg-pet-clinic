89e4ea8a9611da6c976310641be3199a1e0364de
lingtao <lingtaoshui@gmail.com>
2018-11-26 20:28:57 -0600

    Implemented Map impl for Visit Service. Closes #46

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+
+/**
+ * @author LT
+ * @Date on 2018/11/26
+ */
+public interface VisitService extends CrudService<Visit, Long>{
+
+}
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VetMapService.java
@@ -13,11 +13,11 @@ import java.util.Set;
  * @Date on 2018/11/20
  */
 @Service
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+public class VetMapService extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialtyService specialtyService;
 
-    public VetServiceMap(SpecialtyService specialtyService) {
+    public VetMapService(SpecialtyService specialtyService) {
         this.specialtyService = specialtyService;
     }
 
